74227b4e2a5a44d54a708bb1edc5b5e7f99cab08
Sergio Escrivá <sergioescrivap@gmail.com>
2024-11-16 22:46:19 +0100

    Option desplegable inyectado en Activity

--- asociacion/src/main/resources/static/js/ActivityManager.js
+++ asociacion/src/main/resources/static/js/ActivityManager.js
@@ -1,7 +1,8 @@
 
 
 window.onload = function () {
-  ActivityManager.getActivityById(1);
+  ActivityManager.inyectOption()
+  ActivityManager.getActivityById(1)
 };
 
 export class ActivityManager {
@@ -35,9 +36,10 @@ export class ActivityManager {
     activitySel.innerHTML = "";
 
     try {
-      const response = await fetch(`api/activitymember/member/${memberId}`);
+      const response = await fetch(`api/activitymember/activityId/${activityId}`);
       const activities = await response.json();
       activities.forEach((activity) => {
+        console.log(activity)
         activitySel.innerHTML += `
                 <li id="li-activitys-member-${activity.idLong}" data-activity-id="${activity.activityId}">
                     <button class="delete-button" id="li-button-${activity.activityId}"><i class="fas fa-trash"></i></button>
@@ -50,6 +52,35 @@ export class ActivityManager {
     }
   }
 
+
+  static async inyectOption() {
+
+    const activitySel = document.getElementById("activity-select");
+    activitySel.innerHTML = "";
+    activitySel.innerHTML = `<option selected>Selecciona Actividad</option>`
+    try {
+      const response = await fetch(`api/activity`);
+      const activities = await response.json();
+      activities.forEach((activity) => {
+        activitySel.innerHTML += `
+              <option value="${activity.id}">${activity.name}</option>`;
+      });
+    } catch (error) {
+      console.error('Error:', error);
+      activitySel.innerHTML = '<p>Error al cargar las actividades.</p>';
+    }
+
+    // Listening desplegable Option
+    const select1 = document.getElementById('activity-select')
+    let selectedURL = ''
+    const handleChange1 = (event) => {
+      const { value } = event.target
+      console.log(value)
+      this.getActivityById(value);
+    }
+    select1.addEventListener('change', handleChange1)
+
+  }
   static async getActivo(active) {
     var checkbox = document.getElementById('mamagerName');
     checkbox.checked = false
