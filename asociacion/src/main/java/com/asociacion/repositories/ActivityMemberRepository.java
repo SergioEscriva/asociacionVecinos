ef0e7a1c762f3af1a775dfb4d3bd6a8c1d6b1a82
Sergio Escrivá <sergioescrivap@gmail.com>
2024-11-16 23:22:19 +0100

    Añadido Query con búsqueda por actividad y lista join con nombre y apellido

--- asociacion/src/main/java/com/asociacion/repositories/ActivityMemberRepository.java
+++ asociacion/src/main/java/com/asociacion/repositories/ActivityMemberRepository.java
@@ -7,20 +7,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.asociacion.dto.ActivityMemberDTO;
 import com.asociacion.models.ActivityMember;
 import com.asociacion.services.ActivityMemberProjection;
 
 @Repository
 public interface ActivityMemberRepository extends JpaRepository<ActivityMember, Long> {
 
-    @Query("SELECT am.activity.id AS activityId, am.memberId AS memberId, am.id AS idLong, a.name AS activityName " +
+    // @Query("SELECT am.activity.id AS activityId, am.memberId AS memberId, am.id
+    // AS idLong, a.name AS activityName " +
+    // "FROM ActivityMember am " +
+    // "JOIN am.activity a " +
+    // "WHERE am.memberId = :memberId")
+
+    @Query("SELECT am.activity.id AS activityId, am.memberId AS memberId, am.id AS idLong, " +
+            "a.name AS activityName, m.name AS memberName " +
             "FROM ActivityMember am " +
             "JOIN am.activity a " +
+            "JOIN Member m ON m.id = am.memberId " +
             "WHERE am.memberId = :memberId")
     List<ActivityMemberProjection> findActivityMemberDetailsByMemberId(@Param("memberId") Long memberId);
 
-    List<ActivityMember> findByActivityId(Long activityId);
+    @Query("SELECT am.activity.id AS activityId, am.memberId AS memberId, am.id AS idLong, " +
+            "a.name AS activityName, m.name AS memberName, m.lastName1 AS memberApellido " +
+            "FROM ActivityMember am " +
+            "JOIN am.activity a " +
+            "JOIN Member m ON m.id = am.memberId " +
+            "WHERE am.activity.id = :activityId")
+    List<ActivityMemberProjection> findByActivityId(Long activityId);
 
     List<ActivityMember> findByMemberId(Long memberId);
 
--- asociacion/src/main/java/com/asociacion/services/ActivityMemberProjection.java
+++ asociacion/src/main/java/com/asociacion/services/ActivityMemberProjection.java
@@ -8,4 +8,8 @@ public interface ActivityMemberProjection {
     Long getMemberId();
 
     String getActivityName();
+
+    String getMemberName();
+
+    String getMemberApellido();
 }
