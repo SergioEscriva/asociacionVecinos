3da6ce06d1810a17011bc35e8c86b723a1937a17
Sergio Escrivá <sergioescrivap@gmail.com>
2024-11-19 11:29:32 +0100

    Refactorizado Listener, añadido Utility.js

--- asociacion/src/main/resources/static/js/Listeners.js
+++ asociacion/src/main/resources/static/js/Listeners.js
@@ -1,5 +1,5 @@
 import { RequestGet } from "./RequestGet.js";
-import { RequestDel } from "./RequestDel.js";
+import { Utility } from "./Utility.js";
 
 export class Listeners {
     constructor() {
@@ -27,7 +27,7 @@ export class Listeners {
             const activityIdLong = activityInMember.idLong
 
             if (target.classList.contains('delete-button') || target.closest('.delete-button')) {
-                this.delMemberOfActivityMember(memberId, activityIdLong, li);
+                Utility.delMemberOfActivityMember(memberId, activityIdLong, li);
             } else if (target.tagName === 'LABEL') {
                 window.location.href = `./activityIndex.html?activityId=${activityId}`;
             }
@@ -45,7 +45,7 @@ export class Listeners {
             const activityId = document.getElementById('activityId')
 
             if (target.classList.contains('delete-button') || target.closest('.delete-button')) {
-                this.delMemberOfActivity(memberId, activityId.value, li);
+                Utility.delMemberOfActivity(memberId, activityId.value, li);
             } else if (target.tagName === 'LABEL') {
                 const member = await RequestGet.getMemberById(memberId)
 
@@ -54,49 +54,4 @@ export class Listeners {
         });
     }
 
-    static async delMemberOfActivityMember(memberId, activityIdLong, li) {
-        const memberName = document.getElementById('name').value;
-
-        const liElement = document.getElementById('li-activitys-member-' + activityIdLong);
-        const labelElement = liElement.querySelector('label.text-activity');
-        const activityName = labelElement.textContent;
-
-        if (confirm("¿Estás seguro de eleminar a " + memberName + " de la actividad " + activityName + "?")) {
-            try {
-                await RequestDel.delActivityMember(activityIdLong);
-                li.remove();
-                //this.getActivitiesByMemberId(memberId)
-            } catch (error) {
-                console.error("Error al eliminar la actividad:", error);
-                alert("Error al eliminar la actividad. Por favor, intente de nuevo.");
-            }
-        }
-        //const activitySel = document.getElementById("ul-activity-member");
-        //activitySel.innerHTML = "";
-        //await ActivityMemberManager.getActivitiesByMemberId(memberId)
-    }
-
-
-    static async delMemberOfActivity(memberId, activityId, li) {
-        const memberName = li.querySelector('.text-activity').textContent.trim();
-        const activityName = document.getElementById('activityName').value
-
-        const activities = await RequestGet.getActivitiesByMemberId(memberId)
-        const activityInMember = activities.find(activityOne => activityOne.activityId == activityId)
-        const activityIdLong = activityInMember.idLong
-
-        console.log(memberName, activityName)
-        if (confirm("¿Estás seguro de eleminar a " + memberName + " de la actividad " + activityName + "?")) {
-            try {
-                await RequestDel.delActivityMember(activityIdLong);
-                li.remove();
-                //this.getActivitiesByMemberId(memberId)
-            } catch (error) {
-                console.error("Error al eliminar la actividad:", error);
-                alert("Error al eliminar la actividad. Por favor, intente de nuevo.");
-            }
-        } else {
-            return
-        }
-    }
 }
---
+++ asociacion/src/main/resources/static/js/Utility.js
@@ -0,0 +1,51 @@
+import { RequestGet } from "./RequestGet.js";
+import { RequestDel } from "./RequestDel.js";
+
+export class Utility {
+
+    static async delMemberOfActivityMember(memberId, activityIdLong, li) {
+        const memberName = document.getElementById('name').value;
+
+        const liElement = document.getElementById('li-activitys-member-' + activityIdLong);
+        const labelElement = liElement.querySelector('label.text-activity');
+        const activityName = labelElement.textContent;
+
+        if (confirm("¿Estás seguro de eleminar a " + memberName + " de la actividad " + activityName + "?")) {
+            try {
+                await RequestDel.delActivityMember(activityIdLong);
+                li.remove();
+                //this.getActivitiesByMemberId(memberId)
+            } catch (error) {
+                console.error("Error al eliminar la actividad:", error);
+                alert("Error al eliminar la actividad. Por favor, intente de nuevo.");
+            }
+        }
+        //const activitySel = document.getElementById("ul-activity-member");
+        //activitySel.innerHTML = "";
+        //await ActivityMemberManager.getActivitiesByMemberId(memberId)
+    }
+
+
+    static async delMemberOfActivity(memberId, activityId, li) {
+        const memberName = li.querySelector('.text-activity').textContent.trim();
+        const activityName = document.getElementById('activityName').value
+
+        const activities = await RequestGet.getActivitiesByMemberId(memberId)
+        const activityInMember = activities.find(activityOne => activityOne.activityId == activityId)
+        const activityIdLong = activityInMember.idLong
+
+        console.log(memberName, activityName)
+        if (confirm("¿Estás seguro de eleminar a " + memberName + " de la actividad " + activityName + "?")) {
+            try {
+                await RequestDel.delActivityMember(activityIdLong);
+                li.remove();
+                //this.getActivitiesByMemberId(memberId)
+            } catch (error) {
+                console.error("Error al eliminar la actividad:", error);
+                alert("Error al eliminar la actividad. Por favor, intente de nuevo.");
+            }
+        } else {
+            return
+        }
+    }
+}
