cf4d94733618d7a0af9342157073e4098ef265bf
sergisalv <sergisalv@gmail.com>
2024-11-19 12:42:37 +0100

    Intentando cargar página member(Diferente título)

--- asociacion/src/main/resources/static/js/lists.js
+++ asociacion/src/main/resources/static/js/lists.js
@@ -1,20 +1,26 @@
-function changelist(boton){
-    switch(boton){
-        case button1: 
-        window.location.href= 'memberList.html'
-        break; 
-        case button2: 
-        
-        break; 
-        case button3: 
-        break; 
-        case button4: 
-        window.location.href= 'activitiesList.html'
-        break; 
 
-    }
+ function changelist(boton){
+
+        switch(boton){
+            case button1:
+            window.location.href = `./memberList.html?id=1`;
+            break;
+            case button2:
+            window.location.href = `./memberList.html?id=2`;
+            break;
+            case button3:
+            window.location.href = `./memberList.html?id=3`;
+
+            break;
+            case button4:
+            window.location.href= 'activitiesList.html'
+            break;
+
+        }
+
+
+
     
+ }
 
 
-    
-}
