1df31465db93e20a7d59d7a290d7745b185258af
Sergio Escrivá <sergioescrivap@gmail.com>
2024-12-05 23:52:26 +0100

    Ahora los listados muestran como member el attributo de config.

--- asociacion/src/main/resources/static/js/listsManager.js
+++ asociacion/src/main/resources/static/js/listsManager.js
@@ -1,5 +1,6 @@
 import { RequestGet } from './RequestGet.js';
 
+
 export class ListsManager {
 
   constructor() {
@@ -10,30 +11,34 @@ export class ListsManager {
     //Obtener el parámetro activityId de la URL
     //const urlParams = new URLSearchParams(window.location.search);
     // let id = urlParams.get('id');
-
+    const memberAttribute = await RequestGet.getConfigById(3)
     const id = document.body.getAttribute('data-page-selection');
 
     const title = document.getElementById('txtTitleList');
     let response = "";
     switch (id) {
       case 'button1':
-        title.textContent = 'Listado de Miembros Completo'
+        title.textContent = 'Listado de ' + memberAttribute.attribute + '(S) Completo'
+        document.getElementById('listSocio').textContent = "Nº " + memberAttribute.attribute.toUpperCase()
         response = await RequestGet.getAllMembers()
         this.renderList(response)
         break;
       case 'button2':
-        title.textContent = 'Listado de Miembros Activos'
+        title.textContent = 'Listado de ' + memberAttribute.attribute + '(S) Activos'
+        document.getElementById('listSocio').textContent = "Nº " + memberAttribute.attribute.toUpperCase()
         response = await RequestGet.getListMembersActives()
         this.renderList(response)
         break;
       case 'button3':
-        title.textContent = 'Listado de Miembros Inactivos'
+        title.textContent = 'Listado de ' + memberAttribute.attribute + '(S) Inactivos'
+        document.getElementById('listSocio').textContent = "Nº " + memberAttribute.attribute.toUpperCase()
         response = await RequestGet.getListMembersInactives()
         this.renderList(response)
         break;
       case 'button4':
         response = await RequestGet.getActivitys()
         const responseCount = ListsManager.countActivities(response)
+        document.getElementById('activitiesListMemberConfig').textContent = "Nº " + memberAttribute.attribute.toUpperCase() + "(S) REGISTRADAS"
         this.renderActivityList(responseCount)
         break;
       case 'button5':
