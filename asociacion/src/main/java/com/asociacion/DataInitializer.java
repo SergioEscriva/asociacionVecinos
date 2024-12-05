0b31466833bce3bbbdb2d611956cc046b1f1b363
Sergio Escrivá <sergioescrivap@gmail.com>
2024-12-05 19:19:02 +0100

    Fix cambiando atributos por genéricos, archivo DataInitializer

--- asociacion/src/main/java/com/asociacion/DataInitializer.java
+++ asociacion/src/main/java/com/asociacion/DataInitializer.java
@@ -22,8 +22,8 @@ public class DataInitializer implements CommandLineRunner {
         if (configRepository.count() == 0) {
             configRepository.save(new Config("Nombre Organización", true, "Agrupación Vecinal"));
             configRepository.save(new Config("Número Socio Inicial", true, "0"));
-            configRepository.save(new Config("Atributo Socio", true, "Agrupado/a"));
-            configRepository.save(new Config("Cuotas Inpagadas para Inactivo", true, "1"));
+            configRepository.save(new Config("Atributo Socio", true, "Socio/a"));
+            configRepository.save(new Config("Año/s impago para Inactivo", true, "1"));
         }
 
         utils.inactiveNotFee();
