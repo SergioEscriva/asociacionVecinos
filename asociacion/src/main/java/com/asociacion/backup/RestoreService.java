33b01a996f6b2c9f5c3c28be487ef72d8ae7ebba
Sergio Escrivá <sergioescrivap@gmail.com>
2024-12-12 16:36:53 +0100

    Implementado Restore desde config, busqueda de archivo y restaura la base de datos

---
+++ asociacion/src/main/java/com/asociacion/backup/RestoreService.java
@@ -0,0 +1,37 @@
+package com.asociacion.backup;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestoreService {
+
+    public void restoreDatabase(String backupFilePath) throws IOException, InterruptedException {
+        //TODO extraer estos datos a lugar seguro
+        String dbUsername = "root";
+        String dbPassword = "000000";
+        String dbName = "asociacion";
+
+        ProcessBuilder pb = new ProcessBuilder(
+            "mysql",
+            "--user=" + dbUsername,
+            "--password=" + dbPassword,
+            "--host=localhost",
+            "--port=3306",
+            dbName
+        );
+        pb.redirectInput(new File(backupFilePath));
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+        Process process = pb.start();
+        int exitCode = process.waitFor();
+        if (exitCode == 0) {
+            System.out.println("Restauración completada con éxito");
+        } else {
+            System.err.println("Error en la restauración");
+        }
+    }
+}
+
